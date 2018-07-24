0339cc3611d2bcfb8ae4ba3cea0d526330958ff8
YMlion <lmion.yao@gmail.com>
2018-07-24 20:06:56 +0800

    init

---
+++ pages/detail/detail.js
@@ -0,0 +1,29 @@
+//获取应用实例
+const app = getApp()
+var WxParse = require('../../wxParse/wxParse.js');
+
+Page({
+  data: {
+  },
+  //事件处理函数
+
+  onLoad: function (options) {
+    var that = this
+    wx.request({
+      url: 'https://news-at.zhihu.com/api/4/news/' + options.id,
+      success: function (res) {
+        console.log(res)
+        if (res.statusCode != 200) {
+          console.log('load error.')
+          return
+        }
+        wx.setNavigationBarTitle({ title: res.data.title })
+        WxParse.wxParse('detail', 'html', res.data.body, that, 50);
+      }
+    })
+  },
+
+  onPullDownRefresh: function () {
+    wx.stopPullDownRefresh()
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,48 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0,
+    stories: []
+  },
+  //事件处理函数
+
+  onLoad: function () {
+    this.request(this)
+  },
+
+  request: function(that, pullDown = false) {
+    wx.request({
+      url: 'https://news-at.zhihu.com/api/4/news/latest',
+      success: function (res) {
+        if (pullDown) {
+          wx.stopPullDownRefresh()
+        }
+        console.log(res)
+        if (res.statusCode != 200) {
+          console.log('load error.')
+          return
+        }
+        that.setData({
+          stories: res.data.stories
+        })
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+  },
+
+  onPullDownRefresh: function () {
+    this.request(this, true)
+  }
+})
